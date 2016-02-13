9ccd246932832b12b2316f05f90191f8f4a67e05
Jiang Sheng <gisonrg@gmail.com>
2016-02-14 03:23:30 +0800

    Implement getExamples API

--- Symfony/src/Codebender/LibraryBundle/Controller/ApiController.php
+++ Symfony/src/Codebender/LibraryBundle/Controller/ApiController.php
@@ -2,8 +2,8 @@
 
 namespace Codebender\LibraryBundle\Controller;
 
-use Codebender\LibraryBundle\Entity\Example;
-use Codebender\LibraryBundle\Entity\ExternalLibrary;
+use Codebender\LibraryBundle\Entity\Version;
+use Codebender\LibraryBundle\Handler\ApiHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -28,7 +28,6 @@ class ApiController extends Controller
      * Can handle several types of requests, like code fetching, examples fetching, etc.
      *
      * TODO: need to refactor how this work, JsonResponse objects are returned from all over the place inconsistently
-     * @param $version
      * @return JsonResponse
      */
     public function apiHandlerAction()
@@ -58,6 +57,8 @@ class ApiController extends Controller
     private function selectAction($content)
     {
         switch ($content["type"]) {
+            case "getExamples":
+                return $this->getLibraryExamples($content["library"], $content["version"]);
             default:
                 return ['success' => false, 'message' => 'No valid action requested'];
         }
@@ -80,5 +81,140 @@ class ApiController extends Controller
         }
 
         return true;
+    }
+
+    private function getLibraryExamples($library, $version)
+    {
+        $exists = $this->getLibraryType($library);
+        if ($exists['success'] !== true) {
+            return $exists;
+        }
+
+        // TODO: use a default version if version is not given in the request
+        $hasVersion = $this->getVerionForLibrary($library, $version);
+        if ($hasVersion['success'] !== true) {
+            return $hasVersion;
+        }
+
+        /* @var Version $versionObject */
+        $versionObject = $hasVersion['data'];
+
+        $examples = array();
+        $path = "";
+        /*
+         * Assume the requested library is an example
+         */
+        $path = $this->container->getParameter('builtin_libraries') . "/examples/" . $library;
+        if ($exists['type'] == 'external') {
+            $path = $this->container->getParameter('external_libraries_new') . '/' . $library . '/' . $versionObject->getFolderName();
+        }
+        if ($exists['type'] == 'builtin') {
+            $path = $this->container->getParameter('builtin_libraries') . "/libraries/" . $library;
+        }
+        $inoFinder = new Finder();
+        $inoFinder->in($path);
+        $inoFinder->name('*.ino')->name('*.pde');
+
+        foreach ($inoFinder as $example) {
+            $files = array();
+
+            $content = (!mb_check_encoding($example->getContents(), 'UTF-8')) ? mb_convert_encoding($example->getContents(), "UTF-8") : $example->getContents();
+            $pathInfo = pathinfo($example->getBaseName());
+            $files[] = array(
+                "filename" => $pathInfo['filename'] . '.ino',
+                "content" => (!mb_check_encoding($content, 'UTF-8')) ? mb_convert_encoding($content, "UTF-8") : $content
+            );
+
+            // TODO: Not only .h and .cpp files in Arduino examples
+            $notInoFilesFinder = new Finder();
+            $notInoFilesFinder->files()->name('*.h')->name('*.cpp');
+            $notInoFilesFinder->in($path . "/" . $example->getRelativePath());
+
+            foreach ($notInoFilesFinder as $nonInoFile) {
+                $files[] = array(
+                    "filename" => $nonInoFile->getBaseName(),
+                    "content" => (!mb_check_encoding($nonInoFile->getContents(), 'UTF-8')) ? mb_convert_encoding($nonInoFile->getContents(), "UTF-8") : $nonInoFile->getContents()
+                );
+            }
+
+            $dir = preg_replace('/[E|e]xamples\//', '', $example->getRelativePath());
+            $dir = str_replace($pathInfo['filename'], '', $dir);
+            $dir = str_replace('/', ':', $dir);
+            if ($dir != '' && substr($dir, -1) != ':') {
+                $dir .= ':';
+            }
+
+
+            $examples[$dir . $pathInfo['filename']] = $files;
+        }
+        return ['success' => true, 'version' => $version,'examples' => $examples];
+    }
+
+    private function getLibraryType($library)
+    {
+        /* @var ApiHandler $handler */
+        $handler = $this->get('codebender_library.apiHandler');
+
+        /*
+         * Each library's type can be either external () ..
+         */
+        $isExternal = json_decode($handler->checkIfExternalExists($library), true);
+        if ($isExternal['success']) {
+            return ['success' => true, 'type' => 'external'];
+        }
+
+        /*
+         * .. or builtin (SD, Ethernet, etc) ...
+         */
+        $isBuiltIn = json_decode($handler->checkIfBuiltInExists($library), true);
+        if ($isBuiltIn['success']) {
+            return ['success' => true, 'type' => 'builtin'];
+        }
+
+        /*
+         * .. or example (01.Basics, etc)
+         */
+        $isExample = json_decode($handler->checkIfBuiltInExampleFolderExists($library), true);
+        if ($isExample['success']) {
+            return ['success' => true, 'type' => 'example'];
+        }
+
+        // Library was not found, return proper message
+        return ['success' => false, 'message' => 'Library named ' . $library . ' not found.'];
+    }
+
+    /**
+     * Get the requested version of the given library
+     * @param $library
+     * @param $version
+     * @return array
+     */
+    private function getVerionForLibrary($library, $version)
+    {
+        /* @var ApiHandler $handler */
+        $handler = $this->get('codebender_library.apiHandler');
+
+        $versions = $handler->getLibraryVersions($library);
+
+        if (!$versions['success']) {
+            return ['success' => false, 'message' => 'Requested version not found.'];
+        }
+
+        // check if this library contains requested version
+        $result = array_values(
+            array_filter(
+                $versions['data'],
+                function ($obj) use ($version) {
+                    /* @var Version $obj */
+                    return $obj->getVersion() === $version;
+                }
+            )
+        );
+
+        if (empty($result)) {
+            return ['success' => false, 'message' => 'Requested version not found.'];
+        }
+
+        return ['success' => true, 'message' => 'Requested version found.', 'data' => $result[0]];
     }
 }
--- Symfony/src/Codebender/LibraryBundle/Handler/ApiHandler.php
+++ Symfony/src/Codebender/LibraryBundle/Handler/ApiHandler.php
@@ -2,6 +2,7 @@
 
 namespace Codebender\LibraryBundle\Handler;
 
+use Codebender\LibraryBundle\Entity\Library;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -15,5 +16,58 @@ class ApiHandler
     {
         $this->entityManager = $entityManager;
         $this->container = $containerInterface;
+    }
+
+    public function checkIfBuiltInExists($library)
+    {
+        $arduino_library_files = $this->container->getParameter('builtin_libraries') . "/";
+        if (!is_dir($arduino_library_files . "/libraries/" . $library)) {
+            return json_encode(array("success" => false, "message" => "No Library named " . $library . " found."));
+        }
+
+        return json_encode(array("success" => true, "message" => "Library found"));
+    }
+
+    public function checkIfExternalExists($library, $getDisabled = false)
+    {
+        $lib = $this->entityManager
+            ->getRepository('CodebenderLibraryBundle:Library')
+            ->findBy(array('default_header' => $library));
+
+        if (empty($lib) || (!$getDisabled && !$lib[0]->getActive())) {
+            return json_encode(array("success" => false, "message" => "No Library named " . $library . " found."));
+        }
+
+        return json_encode(array("success" => true, "message" => "Library found"));
+    }
+
+    public function checkIfBuiltInExampleFolderExists($library)
+    {
+        $arduinoLibraryFiles = $this->container->getParameter('builtin_libraries') . "/";
+        if (is_dir($arduinoLibraryFiles . "/examples/" . $library)) {
+            return json_encode(array("success" => true, "message" => "Library found"));
+        }
+
+        return json_encode(array("success" => false, "message" => "No Library named " . $library . " found."));
+    }
+
+    /**
+     * Get all versions for a library
+     *
+     * @param $library
+     * @return array
+     */
+    public function getLibraryVersions($library)
+    {
+        /* @var Library $lib */
+        $lib = $this->entityManager
+            ->getRepository('CodebenderLibraryBundle:Library')
+            ->findOneBy(array('default_header' => $library));
+
+        if ($lib === null || !$lib->getActive()) {
+            return ["success" => false, "message" => "No Library named " . $library . " found."];
+        }
+
+        return ["success" => true, "data" => $lib->getVersions()->toArray()];
     }
 }
