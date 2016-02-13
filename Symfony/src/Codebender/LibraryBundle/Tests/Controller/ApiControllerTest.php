f4e7fc3027270e59e22cc65a0401756dfe4729b3
Jiang Sheng <gisonrg@gmail.com>
2016-02-14 03:24:53 +0800

    Add a test case for getExamples API

--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
@@ -2,8 +2,56 @@
 
 namespace Codebender\LibraryBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class ApiControllerTest
+ * @package Codebender\LibraryBundle\Tests\Controller
+ * @SuppressWarnings(PHPMD)
+ */
 
 class ApiControllerTest extends WebTestCase
 {
+    public function testGetExternalLibraryExamples()
+    {
+        $client = static::createClient();
+
+        $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
+
+        $client = $this->postApiRequest(
+            $client,
+            $authorizationKey,
+            '{"type":"getExamples", "version" : "1.0.0", "library" : "MultiIno"}'
+        );
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['success']);
+        $this->assertArrayHasKey('multi_ino_example:methods', $response['examples']);
+        $this->assertArrayHasKey('multi_ino_example', $response['examples']);
+    }
+
+    /**
+     * Use this method for library manager API requests with POST data
+     *
+     * @param Client $client
+     * @param string $authKey
+     * @param string $data
+     * @return Client
+     */
+    private function postApiRequest(Client $client, $authKey, $data)
+    {
+        $client->request(
+            'POST',
+            '/' . $authKey . '/v2',
+            [],
+            [],
+            [],
+            $data,
+            true
+        );
+
+        return $client;
+    }
 }
