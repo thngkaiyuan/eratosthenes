05c7303ac8d871b36f06498eb8c83a52637b97aa
seokmin <bsmmoon@gmail.com>
2016-02-14 12:58:42 +0000

    Copied list test from the defaultController test file

--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
@@ -2,8 +2,75 @@
 
 namespace Codebender\LibraryBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
+    public function testList()
+    {
+        $client = static::createClient();
+
+        $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
+
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"list"}');
+
+        $response = $client->getResponse()->getContent();
+        $response = json_decode($response, true);
+
+        $this->assertArrayHasKey('success', $response);
+        $this->assertTrue($response['success']);
+
+        $this->assertArrayHasKey('categories', $response);
+        $categories = $response['categories'];
+
+        $this->assertArrayHasKey('Examples', $categories);
+        $this->assertNotEmpty($categories['Examples']);
+
+        $this->assertArrayHasKey('Builtin Libraries', $categories);
+        $this->assertNotEmpty($categories['Builtin Libraries']);
+
+        $this->assertArrayHasKey('External Libraries', $categories);
+        $this->assertNotEmpty($categories['External Libraries']);
+
+        $basicExamples = $categories['Examples']['01.Basics']['examples'];
+
+        $this->assertArrayNotHasKey('url', $categories['External Libraries']['MultiIno']);
+        $this->assertArrayHasKey('url', $categories['External Libraries']['DynamicArrayHelper']);
+
+        // Check for a specific, known example
+        $foundExample = array_filter($basicExamples, function($element) {
+            if ($element['name'] == 'AnalogReadSerial') {
+                return true;
+            }
+        });
+
+        $foundExample = array_values($foundExample);
+
+        // Make sure the example was found
+        $this->assertEquals('AnalogReadSerial', $foundExample[0]['name']);
+    }
+
+    /**
+     * Use this method for library manager API requests with POST data
+     *
+     * @param Client $client
+     * @param string $authKey
+     * @param string $data
+     * @return Client
+     */
+    private function postApiRequest(Client $client, $authKey, $data)
+    {
+        $client->request(
+            'POST',
+            '/' . $authKey . '/v1',
+            [],
+            [],
+            [],
+            $data,
+            true
+        );
+
+        return $client;
+    }
 }
