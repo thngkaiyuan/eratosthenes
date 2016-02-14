75af2f52e35f700e606cc735b2ee4e72353b5285
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-02-14 13:45:38 +0800

    Add test for the checkGithubUpdates API

--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
@@ -2,8 +2,79 @@
 
 namespace Codebender\LibraryBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
+    /**
+     * This method tests the checkGithubUpdates API.
+     */
+    public function testCheckGithubUpdates()
+    {
+        $client = static::createClient();
+
+        $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
+
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"checkGithubUpdates"}');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['success']);
+        $this->assertEquals('1 external libraries need to be updated', $response['message']);
+        /*
+         * DynamicArrayHelper library's last commit is not the same as its origin.
+         */
+        $this->assertEquals('DynamicArrayHelper', $response['libraries'][0]['Machine Name']);
+
+        /*
+         * Disabling the library should make it not be returned in the list.
+         */
+        $apiHandler = $this->getService('codebender_library.apiHandler');
+        $apiHandler->toggleLibraryStatus('DynamicArrayHelper');
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"checkGithubUpdates"}');
+
+        $response = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertTrue($response['success']);
+        $this->assertEquals('No external libraries need to be updated', $response['message']);
+    }
+
+    /**
+     * This method returns a given service from its name.
+     *
+     * @param $service
+     * @return the requested service
+     */
+    private function getService($service)
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        return $container->get($service);
+    }
+
+    /**
+     * Use this method for library manager API requests with POST data
+     *
+     * @param Client $client
+     * @param string $authKey
+     * @param string $data
+     * @return Client
+     */
+    private function postApiRequest(Client $client, $authKey, $data)
+    {
+        $client->request(
+            'POST',
+            '/' . $authKey . '/v2',
+            [],
+            [],
+            [],
+            $data,
+            true
+        );
+
+        return $client;
+    }
 }
