cc3e92ac8be5db649a9e6f764a2cb863f01abb72
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-02-14 16:50:37 +0800

    Add test for getVersions API

--- Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
+++ Symfony/src/Codebender/LibraryBundle/Tests/Controller/ApiControllerTest.php
@@ -2,8 +2,107 @@
 
 namespace Codebender\LibraryBundle\Tests\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
+    /**
+     * This method tests the getVersions API.
+     */
+    public function testGetVersions()
+    {
+        // Test successful getVersions calls
+        $this->assertSuccessfulGetVersions('default', ['1.0.0', '1.1.0']);
+        $this->assertSuccessfulGetVersions('DynamicArrayHelper', ['1.0.0']);
+        $this->assertSuccessfulGetVersions('HtmlLib', []);
+
+        // Test invalid getVersions calls
+        $this->assertFailedGetVersions('nonExistentLib');
+        $this->assertFailedGetVersions('');
+        $this->assertFailedGetVersions(null);
+    }
+
+    /**
+     * This method checks if the response of a single getVersions
+     * API call is successful and returns the correct versions.
+     *
+     * @param $defaultHeader
+     * @param $expectedVersions
+     */
+    private function assertSuccessfulGetVersions($defaultHeader, $expectedVersions)
+    {
+        $response = $this->postGetVersionsApi($defaultHeader);
+        $this->assertEquals(true, $response['success']);
+        $this->assertArrayHasKey('versions', $response);
+        $this->assertTrue($this->areSimilarArrays($expectedVersions, $response['versions']));
+    }
+
+    /**
+     * This method checks if the response of a single getVersions
+     * API call is unsuccessful.
+     *
+     * @param $defaultHeader
+     */
+    private function assertFailedGetVersions($defaultHeader)
+    {
+        $response = $this->postGetVersionsApi($defaultHeader);
+        $this->assertEquals(false, $response['success']);
+    }
+
+    /**
+     * This method checks if two arrays, $array1 and $array2,
+     * has the same elements.
+     *
+     * @param $array1
+     * @param $array2
+     * @return bool
+     */
+    private function areSimilarArrays($array1, $array2)
+    {
+        $arrayDiff1 = array_diff($array1, $array2);
+        $arrayDiff2 = array_diff($array2, $array1);
+        $totalDifferences = array_merge($arrayDiff1, $arrayDiff2);
+
+        return empty($totalDifferences);
+    }
+
+    /**
+     * Use this method for library manager API requests with POST data
+     *
+     * @param Client $client
+     * @param string $authKey
+     * @param string $data
+     * @return Client
+     */
+    private function postApiRequest(Client $client, $authKey, $data)
+    {
+        $client->request(
+            'POST',
+            '/' . $authKey . '/v2',
+            [],
+            [],
+            [],
+            $data,
+            true
+        );
+
+        return $client;
+    }
+
+    /**
+     * This method submits a POST request to the getVersions API
+     * and returns its response.
+     *
+     * @param $defaultHeader
+     * @return $response
+     */
+    private function postGetVersionsApi($defaultHeader)
+    {
+        $client = static::createClient();
+        $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
+        $client = $this->postApiRequest($client, $authorizationKey, '{"type":"getVersions","library":"' . $defaultHeader . '"}');
+        $response = json_decode($client->getResponse()->getContent(), true);
+        return $response;
+    }
 }
